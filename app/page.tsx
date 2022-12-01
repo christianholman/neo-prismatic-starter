e5b9875966524b0f3f2dee407322e0a557523430
Christian Holman <christian@einherjer.Home>
2022-12-01 09:52:49 +0000

    Add Twitter auth with Planetscale

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+
 export default function Home() {
   return (
     <div>
       <h1 className="font-bold text-xl">Neo Prismatic Starter</h1>
+      <button onClick={() => signIn()}>Signin</button>
     </div>
   );
 }
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,17 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import NextAuth from "next-auth";
 import prisma from "../../../lib/prismadb";
+import TwitterProvider from "next-auth/providers/twitter";
 
 // Configure NextAuth https://next-auth.js.org/configuration
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID || "",
+      clientSecret: process.env.TWITTER_CLIENT_SECRET || "",
+      version: "2.0",
+    }),
+  ],
 });
